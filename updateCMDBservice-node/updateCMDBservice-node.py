cef56642b19435fb52b686474143c97af417bb58
Yonghao Jiang <875134940@qq.com>
2019-09-18 15:36:45 +0800

    replace('\n','')

--- updateCMDBservice-node/updateCMDBservice-node.py
+++ updateCMDBservice-node/updateCMDBservice-node.py
@@ -24,20 +24,27 @@ for line in open("iplist.txt"):
     port=int(l[3])
     nodelist=l[7:]
 
+    if port==80:
+        nodelist =['10.247.11.172','10.247.11.164','10.247.11.184','10.247.11.185','10.247.11.173','10.247.11.2','10.247.11.199','10.247.11.162','10.247.11.188','10.247.11.195','10.247.14.107']
+
     if '' in nodelist:
         nodelist.remove('')
 
     serviceurl='http://op.kdweibo.cn:8222/v2/service/node/upserts'
     node=[]
     for i in nodelist:
+        i=i.replace('\n','')
         d={"port":port,"ip":i}
         node.append(d)
-    data={"service": service, "nodes": node}
 
+    data = {"service": service, "nodes": node}
     jsondata=json.dumps(data)
-    jsondata
+
     print(jsondata)
 
-    s.post(serviceurl,data=jsondata,headers=headers)
+    rst=s.post(serviceurl,data=jsondata,headers=headers)
+    print (rst.status_code)
+    if rst.status_code != 200:
+        print (service,rst.json())
 
 
